96dce75d0b0539e4b56e4a729aecc1697da4d11d
Aleksey Sadykov <fixerteam@yandex.ru>
2021-08-11 13:02:13 +0400

    chore: extend eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  extends: [
+    '@react-native-community',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    semi: 0,
+    'react/jsx-curly-brace-presence': [
+      2,
+      { props: 'never', children: 'ignore' },
+    ],
+    'react-hooks/exhaustive-deps': 'warn',
+    'eol-last': 1,
+  },
+  globals: {
+    Intl: 'readonly',
+  },
+}
